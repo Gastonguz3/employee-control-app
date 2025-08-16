8a8662e946517076fd4d18b0113a718ac139e191
Gastonguz3 <gastonezguzman@gmail.com>
2025-08-16 18:34:23 -0300

    Cambiando a HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/employee-control-app">
+    <HashRouter basename="/employee-control-app">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )

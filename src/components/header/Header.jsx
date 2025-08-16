c271170c1c4090134ab20e6130a93c54bcb716e8
Gastonguz3 <gastonezguzman@gmail.com>
2025-08-16 18:57:49 -0300

    Arreglo de links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
+import { Link } from "react-router-dom"; 
 import "./Header.css"
 
 function Header() {
@@ -6,12 +7,12 @@ function Header() {
     <>
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <strong>Employee Control App</strong>
           </Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/employee">Post Employee</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/employee">Post Employee</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
